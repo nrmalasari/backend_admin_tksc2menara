58eeeb815935b80ee0aef4c250c74331eae283d7
nrmalasari <nirmalamala1311@gmail.com>
2026-01-07 15:21:54 +0800

    tambahan

--- app/Filament/Resources/FasilitasResource/Pages/ListFasilitas.php
+++ app/Filament/Resources/FasilitasResource/Pages/ListFasilitas.php
@@ -13,7 +13,9 @@ class ListFasilitas extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->label('Tambah Fasilitas')
+                ->icon('heroicon-o-plus-circle'),
         ];
     }
 }
